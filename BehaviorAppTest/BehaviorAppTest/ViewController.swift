6a3b35369d3fc2f312c973919c68dc8668720c98
Joao Ricardo Dos Santos Silva <joao.silva@infraclearsalecom.onmicrosoft.com>
2023-07-21 11:47:35 -0300

    upload test project

---
+++ BehaviorAppTest/BehaviorAppTest/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  BehaviorAppTest
+//
+//  Created by William Awaji on 22/12/19.
+//  Copyright © 2019 WA. All rights reserved.
+//
+
+import UIKit
+import CSBehavior
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    var locationManager: CLLocationManager!
+    var csbehavior: CSBehavior!
+    //Outlets
+    @IBOutlet weak var txtSessionId: UITextField!
+    @IBOutlet var lbResultStatus: UILabel!
+    @IBOutlet var lbGeoresult: UILabel!
+    
+    
+    //Actions
+    @IBAction func btnCollect(_ sender: UIButton) {
+        if (txtSessionId.text == "") {
+            let newSessionId = csbehavior.generateSessionId();
+            lbResultStatus.text = NSString(format: "Enviado dados:\n App: %@\n Sessão gerada: %@", csbehavior.app!, newSessionId!) as String;
+            print(lbResultStatus.text);
+            csbehavior.collectDeviceInformation(newSessionId);
+          
+                
+            
+            lbResultStatus.numberOfLines = 5;
+            lbResultStatus.lineBreakMode = NSLineBreakMode.byWordWrapping
+        } else {
+            csbehavior.collectDeviceInformation(txtSessionId.text);
+            
+            lbResultStatus.text = NSString(format: "Enviado dados:\n App: %@\n Sessão: %@", csbehavior.app!, txtSessionId.text!) as String
+            lbResultStatus.numberOfLines = 5;
+            lbResultStatus.lineBreakMode = NSLineBreakMode.byWordWrapping
+        }
+        txtSessionId.text = "";
+    }
+    
+    @IBAction func btnGeoLocation(_ sender: Any) {
+        if (CLLocationManager.locationServicesEnabled()) {
+            
+            locationManager = CLLocationManager();
+            
+            let status: CLAuthorizationStatus = CLLocationManager.authorizationStatus();
+                        
+            switch (status) {
+            case CLAuthorizationStatus.authorizedAlways:
+                locationManager.requestLocation();
+                break;
+            case CLAuthorizationStatus.authorizedWhenInUse:
+                locationManager.requestWhenInUseAuthorization();
+                break;
+            default:
+                locationManager.requestWhenInUseAuthorization();
+                break;
+            }
+            
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations.last! as CLLocation
+
+        lbGeoresult.numberOfLines = 6;
+        lbGeoresult.text = String(format: "Latitude: %f\n Longitude: %f\n Altitude: %f, HAccur: %f, VAccur: %f, Floor: %d"
+            , (location.coordinate.latitude)
+            , (location.coordinate.longitude)
+            , location.altitude
+            , location.horizontalAccuracy
+            , location.verticalAccuracy
+            , location.floor?.level ?? "unknwon");
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.txtSessionId.delegate = self;
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        csbehavior = CSBehavior.getInstance("APP_KEY");
+    }
+    
+    
+
+}
+
---
+++ BehaviorAppTest/Pods/CSBehavior/CSBehavior.framework/Constants/CSBConstants.m
@@ -0,0 +1,42 @@
+//
+//  CSBConstantes.m
+//  CSBehavior
+//
+//  Created by Leonardo Santos on 31/08/20.
+//  Copyright © 2020 ClearSale. All rights reserved.
+//
+
+#import "CSBConstants.h"
+
+// In common with Android
+NSString *const CSBVARPlatform = @"Platform";
+NSString *const CSBVARSDKVersion = @"SDKVersion";
+NSString *const CSBVARAdvertisingId = @"AdvertisingId";
+NSString *const CSBVARLocation = @"Location";
+NSString *const CSBVARBatteryInfo = @"BatteryInfo";
+NSString *const CSBVARBattery = @"Battery";
+NSString *const CSBVARLanguageCode = @"LanguageCode";
+NSString *const CSBVARDeviceGeneratedId = @"DeviceGeneratedId";
+NSString *const CSBVARVisitorIdentifierForVendor = @"VisitorIdentifierForVendor";
+NSString *const CSBVARDeviceModel = @"DeviceModel";
+NSString *const CSBVARSystemVersion = @"SystemVersion"; //OSVersion
+NSString *const CSBVAROSVersion = @"OSVersion";
+
+//Exclusive on iOS
+NSString *const CSBVARSystemName = @"SystemName";
+NSString *const CSBVARConnectedVia3G = @"ConnectedVia3G";
+NSString *const CSBVARCarrier = @"Carrier";
+NSString *const CSBVARClosedCaptioningEnabled = @"ClosedCaptioningEnabled";
+NSString *const CSBVARDeviceName = @"DeviceName";
+NSString *const CSBVARFreeDiskSpace = @"FreeDiskSpace";
+NSString *const CSBVARGuidedAccessEnabled = @"GuidedAccessEnabled";
+NSString *const CSBVARInvertColorsEnabled = @"InvertColorsEnabled";
+NSString *const CSBVARJailBroken = @"JailBroken";
+NSString *const CSBVARInstalledKeyboards = @"InstalledKeyboards";
+NSString *const CSBVARLocaleEqualsLanguage = @"LocaleEqualsLanguage";
+NSString *const CSBVARMonoAudioEnabled = @"MonoAudioEnabled";
+NSString *const CSBVARPaymentsAllowed = @"PaymentsAllowed";
+NSString *const CSBVARSSID = @"SSID";
+NSString *const CSBVARVoiceOverRunning = @"VoiceOverRunning";
+NSString *const CSBVARCarrierAllowsVoip = @"CarrierAllowsVoip";
+NSString *const CSBVARConnectedViaWifi = @"ConnectedViaWifi";
